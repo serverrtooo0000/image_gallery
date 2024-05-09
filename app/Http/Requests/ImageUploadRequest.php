9380fec91d54d570084edbc6ec1a41af40ec06c4
[serverrtooo0000] <[serversrr82@gmail.com]>
2024-05-09 20:27:17 +0300

    new

--- app/Http/Requests/ImageUploadRequest.php
+++ app/Http/Requests/ImageUploadRequest.php
@@ -23,8 +23,8 @@ class ImageUploadRequest extends FormRequest
     {
         return [
             
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048' //Проверка формата изоброжений
-            'image' => 'required|array|min:1|max:5' //кол-во
+            'image' => ['required|image|mimes:jpeg,png,jpg,gif|max:2048'], //Проверка формата изоброжений
+            'image' => ['required|array|min:1|max:5'], //кол-во
         ];
         
     }
